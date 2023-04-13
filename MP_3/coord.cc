ce37dfb9785ddc08690e6e99b707a0b2e85754e2
Jack Boller <jackboller1@gmail.com>
2023-04-13 10:16:24 -0600

    compile with synchronizer and coordinator

---
+++ MP_3/coord.cc
@@ -0,0 +1,31 @@
+#include <grpc++/grpc++.h>
+#include<glog/logging.h>
+#define log(severity, msg) LOG(severity) << msg; google::FlushLogFiles(google::severity); 
+
+#include "sns.grpc.pb.h"
+#include "snsCoordinator.grpc.pb.h"
+
+
+int main(int argc, char** argv) {
+
+  
+  
+  std::string port = "3010";
+  
+//   int opt = 0;
+//   while ((opt = getopt(argc, argv, "p:")) != -1){
+//     switch(opt) {
+//       case 'p':
+//           port = optarg;break;
+//       default:
+// 	  std::cerr << "Invalid Command Line Argument\n";
+//     }
+//   }
+  
+  std::string log_file_name = std::string("coord-") + port;
+    google::InitGoogleLogging(log_file_name.c_str());
+    log(INFO, "Logging Initialized. Server starting...");
+  //RunServer(port);
+
+  return 0;
+}
